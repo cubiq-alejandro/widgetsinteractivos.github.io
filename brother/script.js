911c36ee7fd9c83ba38c6250ad00ec006e6a151e
Palmera <efectopalmera@gmail.com>
2018-01-31 19:05:13 -0300

    u

--- brother/script.js
+++ brother/script.js
@@ -2,39 +2,61 @@ var map;
 var items;
 var myPosition;
 var markers = [];
-var infowindows = [];         
-    
-/******************   HELPERS    *******************/    
-function calculateDistance(a,t,n,r){var s=deg2rad(n-a),h=deg2rad(r-t),M=Math.sin(s/2)*Math.sin(s/2)+Math.cos(deg2rad(a))*Math.cos(deg2rad(n))*Math.sin(h/2)*Math.sin(h/2),d=2*Math.atan2(Math.sqrt(M),Math.sqrt(1-M));return parseInt(6371*d)}
-function deg2rad(n){return n*(Math.PI/180)}
+var infowindows = [];
+
+/******************   HELPERS    *******************/
+function calculateDistance(a, t, n, r) {
+    var s = deg2rad(n - a),
+        h = deg2rad(r - t),
+        M = Math.sin(s / 2) * Math.sin(s / 2) + Math.cos(deg2rad(a)) * Math.cos(deg2rad(n)) * Math.sin(h / 2) * Math.sin(h / 2),
+        d = 2 * Math.atan2(Math.sqrt(M), Math.sqrt(1 - M));
+    return parseInt(6371 * d)
+}
+
+function deg2rad(n) {
+    return n * (Math.PI / 180)
+}
 
 // function for dynamic sorting
-function compareValues(key,order='asc'){return function(a,b){if(!a.hasOwnProperty(key)||!b.hasOwnProperty(key)){return 0}
-const varA=(typeof a[key]==='string')?a[key].toUpperCase():a[key];const varB=(typeof b[key]==='string')?b[key].toUpperCase():b[key];let comparison=0;if(varA>varB){comparison=1}else if(varA<varB){comparison=-1}
-return((order=='desc')?(comparison*-1):comparison)}}
-
-function showAll() { 
-    for(var i=0;i<items.length;i++){
-    items[i].marker.setVisible(true); 
+function compareValues(key, order = 'asc') {
+    return function(a, b) {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            return 0
+        }
+        const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+        const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+        let comparison = 0;
+        if (varA > varB) {
+            comparison = 1
+        } else if (varA < varB) {
+            comparison = -1
+        }
+        return ((order == 'desc') ? (comparison * -1) : comparison)
     }
-}       
+}
 
-function setVisibility(id){
-    for(var i=0;i<items.length;i++){
-    items[i].marker.setVisible(false); 
+function showAll() {
+    for (var i = 0; i < items.length; i++) {
+        items[i].marker.setVisible(true);
     }
-    items[id].marker.setVisible(true); 
-}             
+}
+
+function setVisibility(id) {
+    for (var i = 0; i < items.length; i++) {
+        items[i].marker.setVisible(false);
+    }
+    items[id].marker.setVisible(true);
+}
 
 function isEmpty(obj) {
     if (obj == null) return true;
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
+    if (obj.length > 0) return false;
+    if (obj.length === 0) return true;
     for (var key in obj) {
         if (hasOwnProperty.call(obj, key)) return false;
     }
     return true;
-}  
+}
 
 function pinSymbol() {
     return {
@@ -45,110 +67,201 @@ function pinSymbol() {
         strokeWeight: 1,
         scale: 1
     };
-}              
-        
-    // Handlebars Helper
-    Handlebars.registerHelper('ifNotEquals', function(arg1, arg2, options) {
-        return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+}
+
+function render() {
+    // Template
+    var targetContainer = $("#items_layout");
+    var source = $("#mustacheTemplate").html();
+    var template = Handlebars.compile(source);
+    var html = template({
+        items: items
     });
-    Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-    });         
-    /*************************************/             
-    
-    $(document.body).on('click', '.menu_lateral', function() {
+    $(targetContainer).html(html);
+}
+
+function displayMarkers(newPosition) {
+    var infowindow = new google.maps.InfoWindow();
+  
+    // Calcular distancia
+    for (i in items) {
+        if (!isEmpty(newPosition)) {
+            items[i].distancia = calculateDistance(newPosition.lat, newPosition.lng, items[i].latitud, items[i].longitud);
+        }
+    }
+
+    // Ordenar por distancia
+    if (!isEmpty(newPosition)) {
+        items.sort(compareValues('distancia'));
+    } else {
+        items.sort(compareValues('provincia'));
+    }
+    // Fin de calcular distancia
+
+    for (i in items) {
+        // Create marker
+        var marker = new google.maps.Marker({
+            position: {
+                lat: items[i].latitud,
+                lng: items[i].longitud
+            },
+            map: map,
+            title: items[i].nombre,
+            animation: google.maps.Animation.DROP,
+            icon: pinSymbol()
+        });
+        items[i].marker = marker;
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(items[i].nombre + '<br>' + items[i].direccion + '<br>' + items[i].localidad);
+                infowindow.open(map, marker);
+            }
+        })(items[i].marker, i));
+    }
+}
+
+// Handlebars Helper
+Handlebars.registerHelper('ifNotEquals', function(arg1, arg2, options) {
+    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+});
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+/*************************************/
+
+$(document.body).on('click', '.menu_lateral', function() {
     var id = $(this).data('id');
     $('.menu_lateral').removeClass('active');
     $(this).addClass('active');
     setVisibility(id)
-    google.maps.event.trigger(items[id].marker,'click')
+    google.maps.event.trigger(items[id].marker, 'click')
     map.panTo(items[id].marker.getPosition())
-    });         
+});
 
-    
-    function success(pos) {
+
+function success(pos) {
     var crd = pos.coords;
-    console.log('More or less ' + crd.accuracy + ' meters.');         
-    myPosition = {lat: crd.latitude, lng: crd.longitude};         
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: myPosition
-    }); 
-    // Set current position
+    console.log('More or less ' + crd.accuracy + ' meters.');
+
+    myPosition = {
+        lat: crd.latitude,
+        lng: crd.longitude
+    };
+
+    var center = new google.maps.LatLng(myPosition.lat, myPosition.lng);
     var position = new google.maps.Marker({
         position: myPosition,
         map: map,
         title: 'Mi ubicación'
-    });   
-    position.setMap(map); 
-    setMarkers();       
-        
-    };
-    
-    function error(err) {
-    // Si hay error muestra el mapa sin geoposicion
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11,
-        center: {lat: -34.6209276, lng: -58.4458738}
-    }); 
+    });
+
+    map.panTo(center);
+    map.setZoom(13);    
+    position.setMap(map);
+    setMarkers(myPosition);
+};
+
+function error(err) {
     setMarkers();
-    };
+};
 
 
-    function setMarkers() {
+function setMarkers(newPosition) {    
     $.ajax({
         dataType: "json",
         url: "data.json",
-        success: function(i){     
-        var infowindow = new google.maps.InfoWindow();
-        items = i;
-
-        // Calcular distancia
-        for (i in items) {  
-        if(!isEmpty(myPosition)){
-            items[i].distancia = calculateDistance(myPosition.lat,myPosition.lng,items[i].latitud,items[i].longitud);  
-        }      
+        success: function(i) {
+            items = i;
+            displayMarkers(newPosition);
+            render();
         }
+    });
+}
 
-        // Ordenar por distancia
-        if(!isEmpty(myPosition)){
-        items.sort(compareValues('distancia')); 
-        } else {
-        items.sort(compareValues('provincia')); 
-        }               
-        // Fin de calcular distancia
 
-        for (i in items) {  
-        // Create marker
-        var marker = new google.maps.Marker({
-            position: {lat: items[i].latitud, lng: items[i].longitud},
-            map: map,
-            title: items[i].nombre,
-            animation: google.maps.Animation.DROP,                
-            icon: pinSymbol()
-        });  
-        items[i].marker = marker;                
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-            infowindow.setContent(items[i].nombre + '<br>' + items[i].direccion + '<br>' + items[i].localidad );
-            infowindow.open(map, marker);
-            }
-        })(items[i].marker, i));                
-        }       
+// Run
+$(document).ready(function() {
 
-        // Template
-        var targetContainer = $("#items_layout");
-        var source = $("#mustacheTemplate").html();
-        var template = Handlebars.compile(source);
-        var html = template({items: items});            
-        $(targetContainer).html(html);          
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {
+            lat: -34.6209276,
+            lng: -58.4458738
         }
-    }); 
-    }
+    });   
     
-    // Run
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+
+            //Render nuevo mapa 
+            for (i in items) {
+                items[i].marker.setMap(null);	
+            }
+
+           var newPosition = {
+                lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng()
+            };
+           
+            setMarkers(newPosition);           
+
+        });
+        map.fitBounds(bounds);
+    });    
+
     navigator.geolocation.getCurrentPosition(success, error, {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-    });  
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    });
+});
+
