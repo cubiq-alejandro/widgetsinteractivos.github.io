c902f129eedc411befd02a2ac875d48115022df2
Palmera <efectopalmera@gmail.com>
2018-02-01 11:40:45 -0300

    update

--- brother/script.js
+++ brother/script.js
@@ -5,13 +5,25 @@ var markers = [];
 var infowindows = [];
 
 /******************   HELPERS    *******************/
-function calculateDistance(a, t, n, r) {
-    var s = deg2rad(n - a),
-        h = deg2rad(r - t),
-        M = Math.sin(s / 2) * Math.sin(s / 2) + Math.cos(deg2rad(a)) * Math.cos(deg2rad(n)) * Math.sin(h / 2) * Math.sin(h / 2),
-        d = 2 * Math.atan2(Math.sqrt(M), Math.sqrt(1 - M));
-    return parseInt(6371 * d)
-}
+
+function calculateDistance(lat1,lon1,lat2,lon2,meters) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+
+    if(meters && meters == true){
+        d = d  * 100;
+    }
+
+    return d.toFixed(2);
+  }
 
 function deg2rad(n) {
     return n * (Math.PI / 180)
@@ -87,12 +99,19 @@ function displayMarkers(newPosition) {
     for (i in items) {
         if (!isEmpty(newPosition)) {
             items[i].distancia = calculateDistance(newPosition.lat, newPosition.lng, items[i].latitud, items[i].longitud);
+            if(items[i].distancia < 1){
+                items[i].distanciaVisible = (items[i].distancia * 1000) + " metros"
+            } else {
+                items[i].distanciaVisible = parseInt(items[i].distancia) + " km"
+            }
         }
     }
 
     // Ordenar por distancia
     if (!isEmpty(newPosition)) {
-        items.sort(compareValues('distancia'));
+        items.sort(function(a, b) {
+            return parseFloat(a.distancia) - parseFloat(b.distancia);
+        });        
     } else {
         items.sort(compareValues('provincia'));
     }
@@ -126,6 +145,12 @@ Handlebars.registerHelper('ifNotEquals', function(arg1, arg2, options) {
 });
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+Handlebars.registerHelper('ifGreater', function(arg1, arg2, options) {
+    return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
+});
+Handlebars.registerHelper('ifLower', function(arg1, arg2, options) {
+    return (arg1 > arg2) ? options.fn(this) : options.inverse(this);
 });
 /*************************************/
 
