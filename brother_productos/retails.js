32d3f88dde6338dda1cad7df7931ebed4a39dca9
Your Name <you@example.com>
2018-05-11 11:13:43 -0300

    update

---
+++ brother_productos/retails.js
@@ -0,0 +1,103 @@
+var retailer = [
+    {
+      "id": 1,
+      "nombre": "TEOREMA",
+      "imagen": "retailers/1.png"
+    },  
+    {
+      "id": 2,
+      "nombre": "STAPLES",
+      "imagen": "retailers/2.png"
+    },
+    {
+      "id": 3,
+      "nombre": "MAPLIT",
+      "imagen": "retailers/3.png"
+    },
+    {
+      "id": 4,
+      "nombre": "VARGAS",
+      "imagen": "retailers/4.png"
+    },     
+    {
+      "id": 5,
+      "nombre": "TIENDA OFICIAL MERCADO LIBRE",
+      "imagen": "retailers/5.png"
+    },    
+    {
+      "id": 6,
+      "nombre": "AIR",
+      "imagen": "retailers/6.png"
+    },
+    {
+      "id": 7,
+      "nombre": "ELIT",
+      "imagen": "retailers/7.png"
+    },  
+    {
+      "id": 8,
+      "nombre": "FREE",
+      "imagen": "retailers/8.png"
+    },  
+    {
+      "id": 9,
+      "nombre": "FALABELLA",
+      "imagen": "retailers/9.png"
+    },  
+    {
+      "id": 10,
+      "nombre": "COMPUMUNDO",
+      "imagen": "retailers/10.png"
+    },  
+    {
+      "id": 11,
+      "nombre": "JUMBO",
+      "imagen": "retailers/11.png"
+    },  
+    {
+      "id": 12,
+      "nombre": "NALDO",
+      "imagen": "retailers/12.png"
+    },  
+    {
+      "id": 13,
+      "nombre": "PARDO",
+      "imagen": "retailers/13.png"
+    },  
+    {
+      "id": 14,
+      "nombre": "FRAVEGA",
+      "imagen": "retailers/14.png"
+    },  
+    {
+      "id": 15,
+      "nombre": "MUSIMUNDO",
+      "imagen": "retailers/15.png"
+    },  
+    {
+      "id": 16,
+      "nombre": "GARBARINO",
+      "imagen": "retailers/16.png"
+    },  
+    {
+      "id": 17,
+      "nombre": "RIBEIRO",
+      "imagen": "retailers/17.png"
+    },  
+    {
+      "id": 18,
+      "nombre": "WALMART",
+      "imagen": "retailers/18.png"
+    },  
+    {
+      "id": 19,
+      "nombre": "CETROGAR",
+      "imagen": "retailers/19.png"
+    },  
+    {
+      "id": 20,
+      "nombre": "COTO",
+      "imagen": "retailers/20.png"
+    }             
+    
+  ]      
