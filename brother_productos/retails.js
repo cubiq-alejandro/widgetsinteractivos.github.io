db6ff55a28d28b568fea724b7901a3e4de247ec2
Your Name <you@example.com>
2018-05-11 11:40:23 -0300

    update

--- brother_productos/retails.js
+++ brother_productos/retails.js
@@ -52,52 +52,52 @@ var retailer = [
     {
       "id": 11,
       "nombre": "JUMBO",
-      "imagen": "retailers/11.png"
+      "imagen": "retailers/11.jpg"
     },  
     {
       "id": 12,
       "nombre": "NALDO",
-      "imagen": "retailers/12.png"
+      "imagen": "retailers/12.jpg"
     },  
     {
       "id": 13,
       "nombre": "PARDO",
-      "imagen": "retailers/13.png"
+      "imagen": "retailers/13.jpg"
     },  
     {
       "id": 14,
       "nombre": "FRAVEGA",
-      "imagen": "retailers/14.png"
+      "imagen": "retailers/14.jpg"
     },  
     {
       "id": 15,
       "nombre": "MUSIMUNDO",
-      "imagen": "retailers/15.png"
+      "imagen": "retailers/15.jpg"
     },  
     {
       "id": 16,
       "nombre": "GARBARINO",
-      "imagen": "retailers/16.png"
+      "imagen": "retailers/16.jpg"
     },  
     {
       "id": 17,
       "nombre": "RIBEIRO",
-      "imagen": "retailers/17.png"
+      "imagen": "retailers/17.jpg"
     },  
     {
       "id": 18,
       "nombre": "WALMART",
-      "imagen": "retailers/18.png"
+      "imagen": "retailers/18.jpg"
     },  
     {
       "id": 19,
       "nombre": "CETROGAR",
-      "imagen": "retailers/19.png"
+      "imagen": "retailers/19.jpg"
     },  
     {
       "id": 20,
       "nombre": "COTO",
-      "imagen": "retailers/20.png"
+      "imagen": "retailers/20.jpg"
     }             
     
   ]      
