a3ccb79d95bd26d71a631679674a0b6839eea2d2
Palmera <efectopalmera@gmail.com>
2018-07-03 11:01:28 -0300

    updat

---
+++ inktank/products.js
@@ -0,0 +1,122 @@
+var productos = [
+    {
+        "id": 1,
+        "nombre": "DCP-T310W",
+        "links": [
+          {
+            "retailer": 4,
+            "link": "https://www.gruponucleo.com.ar/producto/impresora-brother-multifuncion-inkjet-dcp-t310w-s-continuo-copia-scan-cis-wifi-27ppm-1200x6000dpi/prod_id=42348/GrupoNucleo.aspx"
+          },
+          {
+            "retailer": 2,
+            "link": "https://www.staples.com.ar/new_product.asp?sku=IMPBHDCPT310&"
+          },
+          {
+            "retailer": 5,
+            "link": "https://articulo.mercadolibre.com.ar/MLA-629898599-impresora-multifuncion-brother-dcp-t310-sistema-continuo-_JM"
+          },
+          {
+            "retailer": 6,
+            "link": "https://unitronic.com.ar/producto/brother-dcp-t310/"
+          }                                             
+        ]
+    },  
+    {
+        "id": 2,
+        "nombre": "DCP-T510W",
+        "links": [
+          {
+            "retailer": 4,
+            "link": "https://www.gruponucleo.com.ar/producto/impresora-brother-multifuncion-inkjet-dcp-t510w-s-continuo-copia-scan-cis-wifi-27ppm-1200x6000dpi/prod_id=42349/GrupoNucleo.aspx"
+          },           
+          {
+            "retailer": 2,
+            "link": "https://www.staples.com.ar/new_product.asp?sku=IMPBHDCPT510W&tracking=COLECCION_ID_407&"
+          },          
+          {
+            "retailer": 8,
+            "link": "http://www.e-teorema.com/producto.php/1040225/multifuncion_inkjet_brother_inktank_dcp-t510w"
+          },
+          {
+            "retailer": 12,
+            "link": "http://www.naldo.com.ar/impresora-dcpt510w-multifuncion-inktank.html"
+          },           
+          {
+            "retailer": 5,
+            "link": "https://articulo.mercadolibre.com.ar/MLA-636454238-impresora-multifuncion-brother-dcp-t510w-sistema-continuo-_JM"
+          },          
+          {
+            "retailer": 6,
+            "link": "https://unitronic.com.ar/producto/brother-dcp-t510w/"
+          }                                                                                                 
+        ]
+    },      
+    {
+        "id": 3,
+        "nombre": "DCP-T710W",
+        "links": [    
+          {
+            "retailer": 4,
+            "link": "https://www.gruponucleo.com.ar/producto/impresora-brother-multifuncion-inkjet-dcp-t710w-s-continuo-copia-scan-wifi-27ppm-1200x6000dpi/prod_id=42352/GrupoNucleo.aspx"
+          },
+          {
+            "retailer": 2,
+            "link": "https://www.staples.com.ar/new_product.asp?sku=IMPBHDCPT710W&"
+          }, 
+          {
+            "retailer": 12,
+            "link": "http://www.naldo.com.ar/impresora-dcpt710w-multifuncion-inktank.html"
+          },
+          {
+            "retailer": 6,
+            "link": "https://unitronic.com.ar/producto/dcp-t710w/"
+          }                                                     
+        ]
+      },
+      {
+        "id": 4,
+        "nombre": "CM300",
+        "links": [
+          {
+            "retailer": 1,
+            "link": "http://www.e-teorema.com/producto.php/801089/plotter_brother_scanncut_cm300"
+          },
+          {
+            "retailer": 21,
+            "link": "mailto:seminarios@brother.com.ar?subject=HotSale%20CM300&body=Por%20favor%20completá%20en%20este%20email%20tu%20nombre%20y%20apellido%20y%20nos%20pondremos%20en%20contacto%20a%20la%20brevedad!"
+          }              
+        ]
+      },    
+      {
+        "id": 5,
+        "nombre": "CS6000i",
+        "links": [
+          {
+            "retailer": 21,
+            "link": "mailto:seminarios@brother.com.ar?subject=HotSale%20CS6000i&body=Por%20favor%20completá%20en%20este%20email%20tu%20nombre%20y%20apellido%20y%20nos%20pondremos%20en%20contacto%20a%20la%20brevedad!"
+          }                                              
+        ]
+      },
+      {
+        "id": 6,
+        "nombre": "PE430",
+        "links": [
+          {
+            "retailer": 1,
+            "link": "http://www.e-teorema.com/producto.php/776742/bordadora_brother_pe430"
+          }                                                    
+        ]
+      },     
+      {
+        "id": 7,
+        "nombre": "PE7700",
+        "descuento": "25%",
+        "links": [
+          {
+            "retailer": 21,
+            "link": "mailto:seminarios@brother.com.ar?subject=HotSale%20PE7700&body=Por%20favor%20completá%20en%20este%20email%20tu%20nombre%20y%20apellido%20y%20nos%20pondremos%20en%20contacto%20a%20la%20brevedad!"
+          }                                                       
+        ]
+      }                            
+
+    ]
